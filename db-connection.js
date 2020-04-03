685e6a47bb8ea5b5d3b9b8051342924f918ac851
MikeKeane <19keane86@gmail.com>
2020-04-03 11:07:15 +0100

    large update

--- db-connection.js
+++ db-connection.js
@@ -1,199 +1,86 @@
-const mysql = require('mysql');
-const config = require('./config/dbconfig.js');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-let pool = mysql.createPool({
-    host: config.host,
-    port: config.port,
-    user: config.user,
-    password: config.password,
-    database: config.database
+
+mongoose.connect('mongodb://localhost:27017/restaurant', {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: true});
+
+
+const MenuItemSchema = new Schema({
+    item_id: {
+        type: String,
+        unique: true
+    },
+    name: {
+        type: String
+    },
+    price: {
+        type: Number,
+        min: 0
+    }
 });
 
-//insert new menu item
-module.exports.insertNewMenu = (data) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            conn.query('INSERT INTO menus SET ?', data, (err, results) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject({message: 'Failed to insert menu'});
-                }
-            });
-        });
-    });
-};
-
-module.exports.insertNewItem = (data) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            conn.query('INSERT INTO items SET ?', data, (err, results) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject({message: 'Failed to insert item'});
-                }
-            });
-        });
-    });
-};
-
-//select all menus
-module.exports.getMenus = (data) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            let sql = 'SELECT * FROM menus';
-
-            if(data && data.name && data.name !== '') {
-                sql += ' WHERE ?';
-            }
-
-
-            conn.query(sql, data, (err, results) => {
-                if (err) {
-                    console.error('Problem executing query');
-                    conn.release();
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject('');
-                }
-            });
-        });
-    });
-};
-
-
-
-//EXAMPLES
-module.exports.selectExample = () => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            conn.query('SELECT * FROM activeSessions', (err, results) => {
-                if (err) {
-                    console.error('Problem executing query');
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject('');
-                }
-            });
-        });
-    });
-};
-
-module.exports.insertExample = (key, username) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            conn.query(`INSERT INTO activeSessions (sessionKey, username) VALUES ('${key}', '${username}')`, (err, results) => {
-                if (err) {
-                    console.error('Problem inserting active session');
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject('');
-                }
-            });
-        });
-    });
-};
-
-module.exports.deleteExample = (id, key) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            conn.query(`DELETE FROM activeSessions WHERE id = ${id} AND sessionKey = '${key}'`, (err, results) => {
-                if (err) {
-                    console.error('Problem deleting active session');
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject('');
-                }
-            });
-        });
-    });
-};
-
-//quotation related queries
-module.exports.insertExample = (customerDetails) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if (err) throw err;
-
-            conn.query(`INSERT INTO quotations (customerForename, customerSurname, customerTelephoneNumber, customerEmailAddress) 
-                              VALUES ('${customerDetails.forename}', '${customerDetails.surname}', '${customerDetails.tel}', '${customerDetails.email}')`, (err, results) => {
-                if (err) {
-                    console.error('Problem inserting active session');
-                    return;
-                }
-
-                if (results) {
-                    resolve(results);
-
-                    conn.release();
-                } else {
-                    reject('');
-                }
-            });
-        });
-    });
-};
-
-pool.on('error', (err) => {
-    console.log(err.code);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-        pool = mysql.createConnection({
-            host: config.host,
-            port: config.port,
-            user: config.user,
-            password: config.password,
-            database: config.database
-        });
+const MenuSchema = new Schema({
+    menu_id: {
+        type: Number,
+        unique: true
+    },
+    name: {
+        type: String,
+        unique: true
+    },
+    items: {
+        type: [MenuItemSchema],
+        default: []
     }
-})
+});
+
+const OrderItemSchema = new Schema({
+    quantity: {
+        type: Number,
+        default: 1
+    },
+    item_id: {
+        type: String,//must match item_id of a valid menu_item
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const OrderSchema = new Schema({
+    order_id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    order_items: [OrderItemSchema],
+    paid: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const UserSchema = new Schema({
+    user_id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = {
+    Menus: mongoose.model('Menu', MenuSchema),
+    MenuItems: mongoose.model('MenuItem', MenuItemSchema),
+    Orders: mongoose.model('Order', OrderSchema),
+    OrderItems: mongoose.model('OrderItem', OrderItemSchema),
+    Users: mongoose.model('Users', UserSchema)
+};
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const io = require('socket.io')(server);
 
 server.listen(port, console.log('Server running on port: ' + port));
+
 const db = require('./db-connection.js');
 
 const bodyParser = require('body-parser');
@@ -25,112 +26,285 @@ app.use(sessionMiddleware);
 
 app.set('view engine', 'ejs');
 
-//check for active session (i.e. user is logged in)
-let sessionIsActive = sesh => {
-    return (sesh.active && sesh.active === true);
-};
+//set up socket connections
+var kitchenIO = io.of('/kitchen');
+kitchenIO.on("disconnect", function() {
+    alert("Socket disconnected, order will not be sent via live connection");
 
-//ROUTING
-//renders the watier view for taking orders
-app.get('/take-order', (req, res) => {
-    res.render('pages/take-order');
+    //TODO: try to reconnect to socket
 });
 
-
-
-io.on('connection', (socket) => {
-    console.log('Socket connected');
+kitchenIO.on('connection', (socket) => {
+    console.log('Kitchen connection opened');
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 
     socket.on('newOrder', (data) => {
         io.emit('newOrder', data);
-    })
+    });
 });
 
-app.post('/send-order', (req, res) => {
-    //TODO: need to put into database, on receiving order in database then emit event, on error return error to user
-    //io.emit('newOrder', req.body);
+var menusIO = io.of('/menus');
+menusIO.on("disconnect", function() {
+    alert("Lost connection to socket.  You will need to refresh any updates");
 
+    //TODO: try to reconnect to socket
 });
 
-app.get('/generate-bill', (req, res) => {
+menusIO.on('connection', (socket) => {
+    console.log('Menus connection opened');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
+//TODO:may need wrapper to protect scope variables
+//server utility functions
+const sessionIsActive = s => {
+    return s.active && s.active === true;
+};
+//ROUTING
+
+//FOR TESTING - TO BE DELETED
+app.get('/test', (req, res) => {
 
     res.end();
 });
 
+//renders the waiter view for taking orders
+app.get('/waiter', (req, res) => {
+    console.log('GET /waiter');
+    res.render('pages/waiter-view', {
+        pageTitle: 'Waiter View | Uni Restaurant'
+    });
+});
 
-app.get('/active-orders', (req, res) => {
-    res.render('pages/active-orders');
+app.get('/kitchen', (req, res) => {
+    console.log('GET /kitchen');
+    res.render('pages/kitchen');
 });
 
 app.get('/counter', (req, res) => {
-    res.render('pages/counter');
+    console.log('GET /counter');
+    res.render('pages/counter', {
+        pageTitle: 'Counter View | Uni Restaurant'
+    });
 });
 
-//ADMIN API
+//endpoints for DB interactions
+app.get('/menus', (req, res) => {
+    db.Menus.find({}).then(menus => {
+        db.MenuItems.find({}).then(menuItems => {
+            res.send(JSON.stringify(menus));
+        }, itemsFailed => {
+            res.status(500).send({err: failed});
+        });
+
+    },failed => {
+        res.status(500).send({err: failed});
+    });
+});
+
+//get new, valid order number
+const newOrderNumber = (cb) => {
+    db.Orders.find({}).sort({order_id: -1}).limit(1).then(result => {
+        if(result.length === 0) {
+            cb(1);
+        } else {
+            cb(+result[0].order_id + 1);
+        }
+    });
+
+};
+
+//create new order
+app.post('/orders', (req, res) => {
+    //look for relevant order information
+    const orderItems = req.body.items;
+    //newOrderNumber();
+    const orderNumber = newOrderNumber(num => {
+        db.Orders.create({
+            order_id: num,
+            order_items: orderItems
+        }).then(success => {
+            res.end();
+        }, error => {
+            res.end();
+        });
+    });
+});
+
+app.get('/orders', (req, res) => {
+    db.Orders.find({}).then(result => {
+        res.end(JSON.stringify(result));
+
+    }, failed => {
+        res.status(500).send({err: failed});
+    })
+});
+
+
+//ADMIN
 app.get('/admin/login', (req, res) => {
-    const sesh = req.session;
+    console.log('GET /admin/login');
 
-    res.render('pages/admin/login', {targetUrl: sesh.targetUrl});
+    if(sessionIsActive(req.session)) {
+        res.redirect('/admin');
+    } else {
+        res.render('pages/admin/login', {pageTitle: 'Admin Login'});
+    }
+
 });
 
-
-//handles login to admin pages/api
+//checks if user is logged in
 app.use('/admin', (req, res, next) => {
-    const targetUrl = '/admin' + req.url;
-
-    let sesh = req.session;
-    //if user name and password sent with request then check it, otherwise directs to login page
-    if(sessionIsActive(sesh)) {
-        delete sesh.targetUrl;
-        next();
-    } else if(req.body.username && req.body.password) {
-
-        //TODO: validate username and password
-        sesh.active = true;
-
-        res.writeHead(302, {
-            Location: sesh.targetUrl
-        });
-
-        res.end();
+    console.log('Check user is logged in');
+    console.log(req.body);
+    if(req.body.username && req.body.password) {
+        //validate username and password
+        if(true) {
+            req.session.active = true;
+            next();
+        } else {
+            req.session.active = false;
+            res.redirect('/admin/login');
+        }
+    } else if(!sessionIsActive(req.session)) {
+        //redirect to login page if not logged in
+        res.redirect('/admin/login');
     } else {
-        sesh.targetUrl = targetUrl;
-
-        res.writeHead(302, {
-            Location: '/admin/login'
-        });
-
-        res.end();
+        next();
     }
 });
 
-//ADMIN USER PAGES
+//receive log in credentials
+app.post('/admin', (req, res) => {
+    console.log('received POST /admin');
+
+    //TODO: validate user and password here
+    res.redirect('/admin');
+});
+
 app.get('/admin', (req, res) => {
-    res.render('pages/admin/home');
+    console.log('GET /admin');
+    res.render('pages/admin/home', {
+        pageTitle: 'Home | Admin Area'
+    });
+});
+
+app.get('/admin/menus', (req, res) => {
+    console.log('GET /admin/menus');
+    db.Menus.find({}).then(menus => {
+        res.render('pages/admin/menus', {
+            pageTitle: 'Menu Options | Admin Area',
+            menus: JSON.stringify(menus)
+        });
+    },failed => {
+        res.status(500).send({err: failed});
+    });
+});
+
+app.get('/admin/users', (req, res) => {
+    console.log('GET /admin/users');
+    db.Menus.find({}).then(menus => {
+        res.render('pages/admin/menus', {
+            pageTitle: 'Menu Options | Admin Area',
+            menus: JSON.stringify(menus)
+        });
+    },failed => {
+        res.status(500).send({err: failed});
+    });
 });
 
 //ADMIN API
-//retrieves specific menu details (name, start, end)
-app.get('/admin/api/menus/:name', (req, res) => {
-    db.getMenus(req.params).then(result => {
-        res.end(JSON.stringify(result));
+//menus
+app.post('/admin/api/menus', (req, res) => {
+    db.Menus.create(req.body).then(success => {
+        menusIO.emit("newMenuAdded", success);
+        res.end('New menu added');
+    }, failed => {
+        return res.status(500).send({error: failed});
     });
 });
 
-//retrieves all menu details (name, start, end)
 app.get('/admin/api/menus', (req, res) => {
-    db.getMenus().then(result => {
+    db.Menus.find({}).then(result => {
         res.end(JSON.stringify(result));
     });
 });
 
-app.post('/admin/menus/:name/:start_hour?/:end_hour?', (req, res) => {
-    db.insertNewMenu(req.params).then(result => {
-        res.end(JSON.stringify(result));
+app.put('/admin/api/menus/:id', (req, res) => {
+    db.Menus.findOneAndUpdate({menu_id: req.params.id}, req.body, {upsert: true}, function(err, doc) {
+        if (err) return res.status(500).send({error: err});
+
+        res.end('Successfully saved.');
+    });
+});
+
+app.delete('/admin/api/menus/:id', (req, res) => {
+    db.Menus.remove({menu_id: req.params.id}).then(deleted => {
+        menusIO.emit("menuDeleted", {id: req.params.id});
+        res.end('Successfully deleted');
     }, err => {
-        res.end(err.message);
+        return res.status(500).send({err: err});
+    });
+});
+
+//menu items
+app.post('/admin/api/menu-items', (req, res) => {
+    const menuId = req.body.menu_id;
+    db.MenuItems.create(req.body).then(menuItem => {
+        res.send('Menu item created');
+        db.Menus.update({
+            menu_id: menuId
+        },{
+            $push: {items: menuItem}
+        }).then(menuUpdated => {
+            let socketData = menuItem.toJSON();
+            socketData.menu_id = menuId;
+            menusIO.emit("newMenuItemAdded", socketData);
+            res.end('Menu Updated');
+        }, updateFailed => {
+            res.status(500).send({err: updateFailed});
+        });
+    }, failed => {
+        res.status(500).send({err: failed});
+    });
+});
+
+app.get('/admin/api/menu-items', (req, res) => {
+    db.MenuItems.find({}).then(result => {
+        res.end(JSON.stringify(result));
+    }, error => {
+        res.status(500).send({err: error});
+    });
+});
+
+app.get('/admin/api/menu-items/:id', (req, res) => {
+    db.MenuItems.find({item_id: req.params.id}).then(result => {
+        res.end(JSON.stringify(result));
+    }, error => {
+        res.status(500).send({err: error});
+    });
+});
+
+app.put('/admin/api/menu-items/:id', (req, res) => {
+    db.MenuItems.findOneAndUpdate({item_id: req.params.id}, req.body, {upsert: true}, function(err, doc) {
+        if (err) return res.status(500).send({error: err});
+
+        res.end('Successfully saved.');
+    });
+});
+
+app.delete('/admin/api/menu-items/:id', (req, res) => {
+    db.MenuItems.findOne({item_id: req.params.id}).then(menuItem => {
+        db.Menus.findOneAndUpdate({items: {$in: menuItem}}, {$pull: {"items": {_id: menuItem._id}}}).then(() => {
+            db.MenuItems.remove({_id: menuItem._id}).then(() => {
+                menusIO.emit("itemDeleted", {id: req.params.id});
+                res.end('Removed ' + menuItem.item_id);
+            });
+        });
     });
 });
